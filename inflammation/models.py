9eb9fce306f64b073678dce95d1d4633f7cfc00a
BradleyMakinson <bradley.makinson@ukaea.uk>
2025-10-29 15:20:25 +0000

    Updated architecture

--- inflammation/models.py
+++ inflammation/models.py
@@ -8,6 +8,9 @@ and each column represents a single day across all patients.
 """
 
 import numpy as np
+import json
+import glob
+import os
 
 
 def load_csv(filename):  
@@ -32,3 +35,70 @@ def daily_min(data):
     """Calculate the daily min of a 2d inflammation data array."""
     return np.min(data, axis=0)
 
+
+def load_json(filename):
+    """Load a numpy array from a JSON document.
+    
+    Expected format:
+    [
+      {
+        "observations": [0, 1]
+      },
+      {
+        "observations": [0, 2]
+      }    
+    ]
+    :param filename: Filename of CSV to load
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        data_as_json = json.load(file)
+        return [np.array(entry['observations']) for entry in data_as_json]
+    
+
+def compute_standard_deviation_by_day(data):
+    means_by_day = map(daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    return daily_standard_deviation
+
+
+def analyse_data(data_source):
+    """Calculates the standard deviation by day between datasets.
+
+    Gets all the inflammation data from CSV files within a directory,
+    works out the mean inflammation value for each day across all datasets,
+    then plots the graphs of standard deviation of these means."""
+    data = data_source.load_inflammation_data()
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
+    return daily_standard_deviation
+
+
+class CSVDataSource:
+    """
+    Loads all the inflammation CSV files within a specified directory.
+    """
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+
+    def load_inflammation_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError(f"No inflammation data CSV files found in path {self.data_dir}")
+        data = map(load_csv, data_file_paths)
+        return list(data)
+    
+
+class JSONDataSource:
+    """
+    Loads all the inflammation JSON files within a specified directory.
+    """
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+
+    def load_inflammation_data(self):
+        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.json'))
+        if len(data_file_paths) == 0:
+            raise ValueError(f"No inflammation data JSON files found in path {self.data_dir}")
+        data = map(load_json, data_file_paths)
+        return list(data)
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,8 +2,12 @@
 
 import numpy as np
 import numpy.testing as npt
+from inflammation.models import analyse_data, daily_mean, compute_standard_deviation_by_day, CSVDataSource, JSONDataSource
+import os
+import math
+import pytest
+from unittest.mock import Mock
 
-from inflammation.models import daily_mean
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -29,3 +33,37 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+
+def test_analyse_data_mock_source():
+  data_source = Mock()
+  mock_data = [[[0, 2, 0]],
+              [[0, 1, 0]]]
+  data_source.load_inflammation_data.return_value = mock_data
+
+  analyse_data(data_source)
+
+
+def test_analyse_data():
+    path = os.path.join( os.getcwd(), "data/")
+    data_source = CSVDataSource(path)
+    result = analyse_data(data_source)
+    expected_output = [0.,0.22510286,0.18157299,0.1264423,0.9495481,0.27118211,
+                       0.25104719,0.22330897,0.89680503,0.21573875,1.24235548,0.63042094,
+                       1.57511696,2.18850242,0.3729574,0.69395538,2.52365162,0.3179312,
+                       1.22850657,1.63149639,2.45861227,1.55556052,2.8214853,0.92117578,
+                       0.76176979,2.18346188,0.55368435,1.78441632,0.26549221,1.43938417,
+                       0.78959769,0.64913879,1.16078544,0.42417995,0.36019114,0.80801707,
+                       0.50323031,0.47574665,0.45197398,0.22070227]
+    npt.assert_allclose(result, expected_output, atol=1e-5)
+
+
+@pytest.mark.parametrize('data,expected_output', [
+    ([[[0, 1, 0], [0, 2, 0]]], [0, 0, 0]),
+    ([[[0, 2, 0]], [[0, 1, 0]]], [0, math.sqrt(0.25), 0]),
+    ([[[0, 1, 0], [0, 2, 0]], [[0, 1, 0], [0, 2, 0]]], [0, 0, 0])
+],
+ids=['Two patients in same file', 'Two patients in different files', 'Two identical patients in two different files'])
+def test_compute_standard_deviation_by_day(data, expected_output):
+
+    result = compute_standard_deviation_by_day(data)
+    npt.assert_array_almost_equal(result, expected_output)
